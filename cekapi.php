754f62b8547c99d18080bc10b369f8040185e910
Irsyad <irsyadrbbn19@gmail.com>
2021-11-02 20:04:35 +0700

    Membuat file cekapi.php & cekgas.php

---
+++ cekapi.php
@@ -0,0 +1,29 @@
+<?php 
+	//Koneksi ke Database
+	include "koneksi.php";
+
+	//Baca data dari tabel tb_sensor
+	$sql = mysqli_query($konek, "SELECT * FROM tb_api ORDER BY id DESC"); //Data terakhir akan berada diatas
+
+	//Membaca data yang paling atas
+	$data = mysqli_fetch_array($sql);
+	$api = $data['api'];
+
+	//uji, apabila nilai belum ada,maka suhu dianggap 1
+	if($api == "") $api  = 1;
+
+
+	
+
+	if ($api == 1){
+	
+		//Respon
+		echo "AMAN";
+		}
+		else if ($api == 0)
+		{
+		//Respon
+		echo "API TERDETEKSI";
+		} 
+
+ ?>
---
+++ cekgas.php
@@ -0,0 +1,24 @@
+<?php 
+	//Koneksi ke Database
+	include "koneksi.php";
+
+	//Baca data dari tabel tb_sensor
+	$sql = mysqli_query($konek, "SELECT * FROM tb_gas ORDER BY id DESC"); //Data terakhir akan berada diatas
+
+	//Membaca data yang paling atas
+	$data = mysqli_fetch_array($sql);
+	$gas = $data['gas'];
+
+	//apabila nilai belum ada,maka suhu dianggap 0
+	if( $gas == "") $gas = 0;
+
+	if ($gas <= "500") {
+  	echo "NORMAL";
+	} 	else if ($gas <= "650") 
+	{
+  	echo "TIDAK NORMAL!";
+	} 	else 
+	{
+  	echo "BERBAHAYA!!!!";
+	}
+?>
